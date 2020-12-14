5746ecb6813e82561b9bdb710c206ea12f6c34bf
Fabio Maestri <64022521+Fabiomaestri@users.noreply.github.com>
2020-12-14 01:09:19 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// navbar toggler small screen
+$(document).ready(function() {
+  $(".navbar-toggler").click(function() {
+    $(".navbar-toggler").toggleClass("change");
+  });
+// adding background and fixed
+  $(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if(position >=149){
+      $('.navbar').addClass('navbar-background');
+      $('.navbar').addClass('fixed-top');
+    }
+    else{
+      $('.navbar').removeClass('navbar-background');
+      $('.navbar').removeClass('fixed-top');
+    }
+  })
+  // smoth scroll
+  $('.nav-link').click(function(link){
+    link.preventDefault();
+
+    let target = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop:$(target).offset().top
+    },3000);
+  })
+
+    // init Isotope
+  let $grid = $('.grid').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    let filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  // layout Isotope after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+  });
+
+
+  
+});
